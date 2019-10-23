7258ea4ca433759db6930001cbb296ad1adbebfb
g-rtavares <54822864+g-rtavares@users.noreply.github.com>
2019-10-22 22:34:17 -0300

    Add files via upload

---
+++ lib/models/player.dart
@@ -0,0 +1,12 @@
+class Player {
+  String name;
+  int score;
+  int victories;
+  Player({this.name, this.score, this.victories});
+  int addScore(int qtd){
+    if(qtd > 0 && score >= 12) return score;
+    else if(qtd < 0 && score <= 0 ) return score;
+    else score += qtd;
+    return score;
+  }
+}
---
+++ lib/views/home_page.dart
@@ -0,0 +1,280 @@
+import 'package:flutter/material.dart';
+import 'package:marcador_truco/models/player.dart';
+import 'package:screen/screen.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+class _HomePageState extends State<HomePage> {
+  Color corBotao = Colors.black;
+  bool tela = false;
+  List<Player> _players = [
+    Player(name: "Nós", score: 0, victories: 0),
+    Player(name: "Eles", score: 0, victories: 0)
+  ];
+  TextEditingController _nameController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  @override
+  void initState() {
+    super.initState();
+    _resetPlayers();
+  }
+  void _resetPlayer({Player player, bool resetVictories = true}) {
+    setState(() {
+      player.score = 0;
+      if (resetVictories) player.victories = 0;
+    });
+  }
+  void _resetPlayers({bool resetVictories = true}) {
+    _resetPlayer(player: _players[0], resetVictories: resetVictories);
+    _resetPlayer(player: _players[1], resetVictories: resetVictories);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple[100],
+        title: Text("Marcador Pontos (Truco!)"),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              if (tela) {
+                setState(() {
+                  Screen.keepOn(false);
+                  tela = false;
+                  corBotao = Colors.deepPurple[300];
+                });
+              } else {
+                setState(() {
+                  Screen.keepOn(true);
+                  tela = true;
+                  corBotao = Colors.deepPurple[50];
+                });
+              }
+            },
+            icon: Icon(
+              Icons.wb_sunny,
+              color: corBotao,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              _showDialog(
+                  title: 'Zerar pontuação',
+                  message:
+                      'Tem certeza que deseja zerar a pontuação?',
+                  confirm: () {
+                    _resetPlayers(resetVictories: false);
+                  });
+            },
+            icon: Icon(Icons.subdirectory_arrow_left),
+          ),
+          IconButton(
+            onPressed: () {
+              _showDialog(
+                  title: 'Novo jogo',
+                  message: 'Tem certeza que deseja começar um novo jogo?',
+                  confirm: () {
+                    _resetPlayers();
+                  });
+            },
+            icon: Icon(Icons.refresh),
+          ),
+        ],
+      ),
+      body: Container(padding: EdgeInsets.all(20.0), child: _showPlayers()),
+    );
+  }
+  Widget _showPlayers() {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        _showPlayerBoard(_players[0]),
+        _showPlayerBoard(_players[1]),
+      ],
+    );
+  }
+  Widget _showPlayerBoard(Player player) {
+    return Expanded(
+      flex: 1,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          _showPlayerName(player),
+          _showPlayerScore(player.score),
+          _showPlayerVictories(player.victories),
+          _showScoreButtons(player),
+        ],
+      ),
+    );
+  }
+  Widget _showPlayerName(Player player) {
+    return GestureDetector(
+      child: Text(
+        player.name.toUpperCase(),
+        style: TextStyle(
+            fontSize: 22.0,
+            fontWeight: FontWeight.w500,
+            color: Colors.deepOrange),
+      ),
+      onTap: () {
+        setState(() {
+          _resetName(player);
+        });
+      },
+    );
+  }
+  void _resetName(Player player) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Trocar nome"),
+            content: Form(
+              key: _formKey,
+              child: TextFormField(
+                decoration: InputDecoration(labelText: 'Novo nome'),
+                controller: _nameController,
+                validator: (text) {
+                  return text.isEmpty ? "Insira um nome!" : null;
+                },
+              ),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("CANCEL"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              FlatButton(
+                child: Text("OK"),
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    Navigator.of(context).pop();
+                    setState(() {
+                      player.name = _nameController.text;
+                    });
+                    _nameController.text = "";
+                  }
+                },
+              ),
+            ],
+          );
+        });
+  }
+  Widget _showPlayerVictories(int victories) {
+    return Text(
+      "Vitórias ( $victories )",
+      style: TextStyle(fontWeight: FontWeight.w300),
+    );
+  }
+  Widget _showPlayerScore(int score) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 52.0),
+      child: Text(
+        "$score",
+        style: TextStyle(fontSize: 120.0),
+      ),
+    );
+  }
+  Widget _buildRoundedButton(
+      {String text, double size = 52.0, Color color, Function onTap}) {
+    return GestureDetector(
+      onTap: onTap,
+      child: ClipOval(
+        child: Container(
+          color: color,
+          height: size,
+          width: size,
+          child: Center(
+              child: Text(
+            text,
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          )),
+        ),
+      ),
+    );
+  }
+  Widget _showScoreButtons(Player player) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        _buildRoundedButton(
+          text: '-1',
+          color: Colors.black.withOpacity(0.1),
+          onTap: () {
+            setState(() {
+              player.addScore(-1);
+            });
+          },
+        ),
+        _buildRoundedButton(
+          text: '+1',
+          color: Colors.deepPurple,
+          onTap: () {
+            setState(() {
+              player.addScore(1);
+            });
+            if (_players[0].score == 11 && _players[1].score == 11) {
+              _showDialog(
+                  title: "Mão de Onze",
+                  message:
+                      "Chegou na mão de onze !!");
+            } else if (player.score == 12) {
+              _showDialog(
+                  title: 'Fim do jogo',
+                  message: '${player.name} ganhou!',
+                  confirm: () {
+                    setState(() {
+                      player.victories++;
+                    });
+                    _resetPlayers(resetVictories: false);
+                  },
+                  cancel: () {
+                    setState(() {
+                      player.addScore(-1);
+                    });
+                  });
+            }
+          },
+        ),
+      ],
+    );
+  }
+  void _showDialog(
+      {String title, String message, Function confirm, Function cancel}) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("CANCEL"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                if (cancel != null) cancel();
+              },
+            ),
+            FlatButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                if (confirm != null) confirm();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
